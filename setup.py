3f72b9aff206436b67a07f2a904b3de6729ef538
ZARIFMAC <uzbzarif@gmail.com>
2022-01-17 14:02:39 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+#Configure the setup
+setup(
+    name='py-pip-test',
+    version='0.1',
+    description='A simple test package',
+    py_modules=['hw'],
+    package_dir={'': 'src'},
+)
---
+++ src/hw.py
@@ -0,0 +1,4 @@
+#Define function, return 'Hello World'
+def hello_world():
+    return 'Hello World'
+    
